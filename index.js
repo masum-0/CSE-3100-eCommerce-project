50f462fd3a24aac159ebda5fbc07972d69028497
Abdullah Al Masum <almasumpqr@gmail.com>
2025-10-24 23:28:20 +0600

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import router from "./routes/userRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
@@ -11,7 +11,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use("/api/users", router);
+app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
