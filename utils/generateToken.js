d42a486299cd5cb8051110f3038d367af784066f
Abdullah Al Masum <almasumpqr@gmail.com>
2025-12-19 20:09:21 +0600

    utils folder removed

--- utils/generateToken.js
+++
@@ -1,6 +0,0 @@
-// file: utils/generateToken.js
-import jwt from "jsonwebtoken"
-
-export const generateToken = userId => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
-}
